2c399f7d5b99dfbc9660d125ee640a8eabb3b255
madahtoxa <madahtoxa@mail.ru>
2022-10-17 21:41:37 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        Employee[] all = new Employee[10];
+        System.out.println();
+        System.out.println(e1.getSalary());
+        e1.setSalary(45000);
+        System.out.println(e1.getSalary());
     }
 }
